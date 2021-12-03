309863f33541c7b16d4d70773449086d276b3e22
ozkalai <melihozkalayy@gmail.com>
2021-12-03 23:39:08 +0300

    init testing environment

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '^@/components/(.*)$': '<rootDir>/components/$1',
+    }
+}
+
+module.exports = createJestConfig(customJestConfig)
