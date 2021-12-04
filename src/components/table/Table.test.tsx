d89c213e3dee691119eaee9feda202eb491daddd
ozkalai <melihozkalayy@gmail.com>
2021-12-04 19:18:52 +0300

    Table component test and style filw [ WIP ]

---
+++ src/components/table/Table.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { Table } from ".";
+
+describe("Table", () => {
+  it("renders a table element", () => {
+    render(<Table />);
+  });
+});
