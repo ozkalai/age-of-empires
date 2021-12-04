32c4d3e105299ea9b1f488fd1c7912f86cb73193
ozkalai <melihozkalayy@gmail.com>
2021-12-04 23:33:06 +0300

    Table components updated

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,72 +1,73 @@
 import React from "react";
-import { useTable } from "react-table";
+import Link from "next/link";
 
-import MaUTable from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import MaUTable from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableContainer from "@mui/material/TableContainer";
+import { Cost, IUnitCommon } from "@/typing/Unit";
+import { Button } from "@material-ui/core";
+
+interface IRow extends Pick<IUnitCommon, "id" | "age" | "name" | "cost"> {}
 
 interface ITable {
-  data: any[];
+  data: IRow[];
 }
 
+const tableCols = ["ID", "Name", "Age", "Cost", ""];
+
+const renderCost = (cost: Cost | null) =>
+  `${cost?.Wood ? `${cost?.Wood} 🪵 ` : ""}${cost?.Food ? `${cost?.Food} 🥩 ` : ""}${
+    cost?.Gold ? `${cost?.Gold} 💰 ` : ""
+  }`;
+
 export const Table: React.FC<ITable> = ({ data }) => {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: "id",
-        accessor: "col1", // accessor is the "key" in the data
-      },
-      {
-        Header: "name",
-        accessor: "col2",
-      },
-      {
-        Header: "age",
-        accessor: "col3", // accessor is the "key" in the data
-      },
-      {
-        Header: "costs",
-        accessor: "col4",
-      },
-    ],
-    [],
-  );
-
-  const data = ["", ""];
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
-
   return (
-    <MaUTable {...getTableProps()}>
-      <TableHead>
-        {headerGroups.map((headerGroup, index) => (
-          <TableRow {...headerGroup.getHeaderGroupProps()} key={index + 1}>
-            {headerGroup.headers.map((column, idx) => (
-              <TableCell {...column.getHeaderProps()} key={idx + 1}>
-                {column.render("Header")}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableHead>
-      <TableBody>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <TableRow {...row.getRowProps()} key={i + 1}>
-              {row.cells.map((cell, j) => {
-                return (
-                  <TableCell {...cell.getCellProps()} key={j + 1}>
-                    {cell.render("Cell")}
-                  </TableCell>
-                );
-              })}
+    <div className="w-full overflow-scroll">
+      <TableContainer>
+        <MaUTable>
+          <TableHead>
+            <TableRow>
+              {tableCols.map((name) => (
+                <TableCell variant="head" key={`head-cell-${name}`}>
+                  {name}
+                </TableCell>
+              ))}
             </TableRow>
-          );
-        })}
-      </TableBody>
-    </MaUTable>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => {
+              return (
+                <TableRow key={`body-row-${row.id}`}>
+                  <TableCell key={`body-cell-${row.id}-id`}>{row.id}</TableCell>
+                  <TableCell key={`body-cell-${row.id}-name`}>
+                    <div className="hidden md:block">{row.name}</div>
+                    <div className="block md:hidden">
+                      <Link href={`/units/${row.id}`}>
+                        <a className="underline">{row.name}</a>
+                      </Link>
+                    </div>
+                  </TableCell>
+                  <TableCell key={`body-cell-${row.id}-age`}>{row.age}</TableCell>
+                  <TableCell key={`body-cell-${row.id}-cost`}>{renderCost(row.cost)}</TableCell>
+                  <TableCell align="left" padding="none" key={`body-cell-${row.id}-cost`}>
+                    <div className="hidden md:block">
+                      <Button className="hidden md:block w-1/2 md:w-full" variant="outlined">
+                        Details
+                      </Button>
+                    </div>
+                  </TableCell>
+                  {/* {row..map((cell, j) => {
+                return <TableCell key={j + 1}>{cell.render("Cell")}</TableCell>;
+              })} */}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </MaUTable>
+      </TableContainer>
+    </div>
   );
 };
