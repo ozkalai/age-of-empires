0bd3238f94eac9360be0d984a4a349314a9e3bce
ozkalai <melihozkalayy@gmail.com>
2021-12-04 12:29:17 +0300

    added nunito font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'nunito': ['nunito', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
