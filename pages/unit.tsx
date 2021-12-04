90895c568c56043eb6fa9d0f7459d9c34195482b
ozkalai <melihozkalayy@gmail.com>
2021-12-04 19:18:15 +0300

    unit page init

--- pages/unit.tsx
+++ pages/unit.tsx
@@ -1,6 +1,22 @@
+import React from "react";
 import type { NextPage } from "next";
+import { Table } from "@/components/table";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import { units } from "@/data/units.json";
 
 const Unit: NextPage = () => {
-  return <div>Unit Page</div>;
+  // tüm sayfada kullanılacak data burda olsun
+  // filtreleme componentlerini burda çağırır datayı handle ederiz
+  // data yı sonrasında table component ına göndreririz
+
+  const data = [];
+
+  return (
+    <>
+      <CssBaseline />
+      <Table data={data} />
+    </>
+  );
 };
 export default Unit;
