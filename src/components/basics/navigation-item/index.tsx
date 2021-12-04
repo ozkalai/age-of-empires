0bba2fe53d7511ac208224a7634c87a7413def7b
ozkalai <melihozkalayy@gmail.com>
2021-12-04 14:58:16 +0300

    Refactor NavigationItem for routing

--- src/components/basics/navigation-item/index.tsx
+++ src/components/basics/navigation-item/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 import styles from "./NavigationItem.module.sass";
 
 interface INavigationItem extends React.AnchorHTMLAttributes<HTMLLIElement> {
   isActive?: boolean;
+  href: string;
 }
 
-export const NavigationItem: React.FC<INavigationItem> = ({ children, className, isActive }) => {
+export const NavigationItem: React.FC<INavigationItem> = ({ children, className, isActive, href }) => {
   return (
     <li title="navigation-item" className={`${styles.navigationItem} ${isActive ? `${styles.active}` : ""}`}>
-      {children}
+      <Link href={href}>
+        <a>{children}</a>
+      </Link>
     </li>
   );
 };
