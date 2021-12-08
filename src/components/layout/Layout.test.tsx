ed4e9ca4fb6e74325e6bf79b0c468e7bddc8b897
Melih Özkalay <75166276+ozkalai@users.noreply.github.com>
2021-12-08 17:28:57 +0300

    Layout unit test fixed

--- src/components/layout/Layout.test.tsx
+++ src/components/layout/Layout.test.tsx
@@ -6,36 +6,14 @@ import { createMockRouter } from "../../../__mocks__/createMockRouter";
 import { RouterContext } from "next/dist/shared/lib/router-context";
 
 describe("Layout", () => {
-  it("renders a logo", () => {
+  it("renders navigation ", () => {
     render(
       <RouterContext.Provider value={createMockRouter({})}>
         <Layout />
       </RouterContext.Provider>,
     );
 
-    const logo = screen.getByText("🛡");
-    expect(logo).toBeTruthy();
-  });
-
-  it("renders Home navigation item", () => {
-    render(
-      <RouterContext.Provider value={createMockRouter({})}>
-        <Layout />
-      </RouterContext.Provider>,
-    );
-
-    const listElement = screen.getByText("Home");
-    expect(listElement).toBeTruthy();
-  });
-
-  it("renders Home navigation item", () => {
-    render(
-      <RouterContext.Provider value={createMockRouter({})}>
-        <Layout />
-      </RouterContext.Provider>,
-    );
-
-    const listElement2 = screen.getByText("Units");
-    expect(listElement2).toBeTruthy();
+    const navigation = screen.getByTestId("navigation");
+    expect(navigation).toBeTruthy();
   });
 });
