a32b644197798aac3527369f902035bae587bcb5
Melih Özkalay <75166276+ozkalai@users.noreply.github.com>
2021-12-08 23:46:27 +0300

    CostFilter logic final

--- pages/units.tsx
+++ pages/units.tsx
@@ -11,44 +11,99 @@ import { Button, ButtonGroup, Box } from "@mui/material";
 
 interface IFilterCosts {
   type: keyof ICost;
-  range: [number, number];
+  range: number[];
+  isChecked: boolean;
+  value: number[];
 }
 
 const ages = ["All", "Dark", "Feudal", "Castle", "Imperial"];
 const costs: IFilterCosts[] = [
-  { type: "Wood", range: [0, 200] },
-  { type: "Food", range: [0, 200] },
-  { type: "Gold", range: [50, 150] },
+  { type: "Wood", range: [0, 200], isChecked: false, value: [0, 200] },
+  { type: "Food", range: [0, 200], isChecked: false, value: [0, 200] },
+  { type: "Gold", range: [50, 150], isChecked: false, value: [50, 150] },
 ];
 
 const Units: NextPage = () => {
   const [units, setUnits] = useState<IUnit[]>(rawData.units as IUnit[]);
   const [selectedAge, setSelectedAge] = useState("All");
-  const [selectedCost, setSelectedCost] = useState<IFilterCosts[]>();
+  const [costFilter, setCostFilter] = useState<IFilterCosts[]>(costs);
 
-  useEffect(() => {
-    if (selectedAge !== "All") {
-      const filteredByAge = rawData.units?.filter((a) => selectedAge === a.age);
-
-      setUnits(filteredByAge as IUnit[]);
-    } else {
-      setUnits(rawData.units as IUnit[]);
-    }
-  }, [selectedAge, selectedCost]);
-
-  const updateSelectedCosts = (costType: keyof ICost, isChecked: boolean) => {
-    if (!isChecked) {
-      const newCosts = costs.filter((c) => c.type === costType);
-      if (selectedCost) {
-        setSelectedCost([...selectedCost, newCosts[0]]);
-      } else {
-        setSelectedCost(newCosts);
-      }
-    } else {
-      const updatedCosts = selectedCost?.filter((c) => c.type !== costType);
-      setSelectedCost(updatedCosts);
+  const getValueCost = (costType: keyof ICost, range: number[], value: any) => {
+    switch (costType) {
+      case "Wood":
+        if (range[0] < value && range[1] > value) return true;
+        break;
+      case "Food":
+        if (range[0] < value && range[1] > value) return true;
+        break;
+      case "Gold":
+        if (range[0] < value && range[1] > value) return true;
+        break;
+      default:
+        return false;
+        break;
     }
   };
+
+  useEffect(() => {
+    let data = [...rawData.units];
+
+    if (selectedAge !== "All") {
+      data = data.filter((a) => selectedAge === a.age);
+    }
+
+    const selectedCosts = costFilter.filter((c) => c.isChecked);
+
+    if (selectedCosts.length > 0) {
+      selectedCosts.forEach(
+        (c) =>
+          (data = data.filter((i) => {
+            // true or false
+
+            if (i.cost) {
+              const val = i.cost[c.type];
+              return getValueCost(c.type, c.value, val);
+            } else {
+              return false;
+            }
+          })),
+      );
+    }
+
+    setUnits(data as IUnit[]);
+  }, [selectedAge, costFilter]);
+
+  const updateSelectedCosts = (costType: keyof ICost) => {
+    setCostFilter((filter) => {
+      return filter.map((f) => {
+        if (f.type === costType) {
+          return {
+            ...f,
+            isChecked: !f.isChecked,
+          };
+        }
+        return f;
+      });
+    });
+  };
+
+  const updateCostRange = (value: number[], costType: keyof ICost) => {
+    setCostFilter((filter) => {
+      return filter.map((f) => {
+        if (f.type === costType) {
+          return {
+            ...f,
+            value,
+          };
+        }
+        return f;
+      });
+    });
+  };
+
+  useEffect(() => {
+    const checkedCosts = costFilter.filter((i) => i.isChecked === true);
+  }, [costFilter]);
 
   return (
     <div className={styles.unitsContainer}>
@@ -75,8 +130,16 @@ const Units: NextPage = () => {
       </div>
       <div className={styles.filter}>
         <span className={styles.heading}>Costs</span>
-        {costs.map((c, i) => (
-          <CostFilter costType={c.type} key={i + 1} range={c.range} updateSelectedCosts={updateSelectedCosts} />
+        {costFilter.map((c, i) => (
+          <CostFilter
+            key={i + 1}
+            isChecked={c.isChecked}
+            costType={c.type}
+            range={c.range}
+            value={c.value}
+            updateSelectedCosts={updateSelectedCosts}
+            updateCostRange={updateCostRange}
+          />
         ))}
       </div>
       <Table data={units} />
