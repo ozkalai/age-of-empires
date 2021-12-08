f63e193c516582635b10f452f2b5a4c6c2fad795
Melih Özkalay <75166276+ozkalai@users.noreply.github.com>
2021-12-09 00:26:41 +0300

    Added title and icon to Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Head from "next/head";
 
 import styles from "@styles/pages/Home.module.sass";
+import React from "react";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.home}>
+      <Head>
+        <title>Age of Empries Units</title>
+        <link rel="icon" href="warrior.png" />
+      </Head>
       <div className={styles.imageWrapper}>
         <Image alt="cover-image" src="/images/age.jpg" layout="fill" objectFit="cover" />
         <div className={styles.name}>
