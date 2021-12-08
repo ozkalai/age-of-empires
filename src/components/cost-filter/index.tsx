1ae5345e17f68a83425edddecf4b74cf4eefeed9
ozkalai <melihozkalayy@gmail.com>
2021-12-08 09:47:42 +0300

    updateSelectedCost passed to CostFilter function

--- src/components/cost-filter/index.tsx
+++ src/components/cost-filter/index.tsx
@@ -5,18 +5,17 @@ import styles from "./CostFilter.module.sass";
 import { Cost } from "../../typing/Unit";
 
 interface ICostFilter {
-  costType: string;
+  costType: keyof Cost;
   range: number[];
-  setUnitsWithCosts: (range: number[], costType: string) => void;
+  updateSelectedCosts: (costType: keyof Cost, isChecked: boolean) => void;
 }
 
-export const CostFilter: FC<ICostFilter> = ({ costType, range, setUnitsWithCosts }) => {
+export const CostFilter: FC<ICostFilter> = ({ costType, range, updateSelectedCosts }) => {
   const [isChecked, setIsChecked] = useState(false);
   const [value, setValue] = useState(range);
 
   const handleChange = (event: Event | React.SyntheticEvent<Element, Event>, newValue: number | number[]) => {
     setValue(newValue as number[]);
-    setUnitsWithCosts(range, costType);
   };
 
   return (
@@ -24,13 +23,14 @@ export const CostFilter: FC<ICostFilter> = ({ costType, range, setUnitsWithCosts
       <Checkbox
         data-testid="cost-check"
         onClick={() => {
+          updateSelectedCosts(costType, isChecked);
           setIsChecked(!isChecked);
         }}
         color="default"
       />
-      <div className={`${styles.range} ${isChecked ? "" : `${styles.checked}`}`}>
+      <div data-testid="wrapper" className={`${styles.range} ${isChecked ? "" : `${styles.checked}`}`}>
         <span className={styles.costType}>{costType}</span>
-        <Box sx={{ width: 300, display: "flex", alignItems: "center" }} marginLeft="20px">
+        <Box data-testid="box" sx={{ width: 300, display: "flex", alignItems: "center" }} marginLeft="20px">
           <Slider
             data-testid="slider"
             sx={{ color: "black" }}
