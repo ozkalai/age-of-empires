292317009e1cfaa15ab38d68e81bfe3cb30878bc
Melih Özkalay <75166276+ozkalai@users.noreply.github.com>
2021-12-08 17:19:56 +0300

    export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from "./basics";
 export { Navigation } from "./navigation";
 export { Layout } from "./layout";
+export { Table } from "./table";
+export { CostFilter } from "./cost-filter";
